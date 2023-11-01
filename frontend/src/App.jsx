7b87556e18c7295917e62c056296cffd1ccea204
Fikri Naufal Hamdi <fikrinaufalh9@gmail.com>
2023-11-02 00:51:22 +0700

    feat: setup react and tailwind project using vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <div className="bg-blue-500 grid place-content-center min-h-screen">
+            <h1 className="text-xl">Hello, world!</h1>
+        </div>
+    );
+}
